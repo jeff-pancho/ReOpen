4530bc5976929ac4a6f7f47b9b7b3e22f81a2783
daffyd-jones <daffydbjones@gmail.com>
2021-03-05 00:14:42 -0800

    - added Business profile page and maps fragment.

---
+++ app/src/main/java/com/example/reopen/BusProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.reopen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_profile);
+    }
+}
